6ab0f7c1ff3660316cebc33474db555eeb541b69
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2019-05-10 10:09:33 +0200

    Move sleep/wakeup to queued export

--- src/Actions/ExportToExcel.php
+++ src/Actions/ExportToExcel.php
@@ -72,35 +72,6 @@ class ExportToExcel extends Action implements FromQuery, WithCustomChunkSize, Wi
     protected $onFailure;
 
     /**
-     * Remove some attributes from this class when serializing,
-     * so the action can be queued as exportable.
-     * Serialize the request, so we keep information about
-     * the resource and lens in the queued jobs.
-     *
-     * @return array
-     */
-    public function __sleep()
-    {
-        if (!$this->request instanceof SerializedRequest) {
-            $this->request = SerializedRequest::serialize($this->request);
-        }
-
-        return ['headings', 'except', 'only', 'onlyIndexFields', 'request', 'resource'];
-    }
-
-    /**
-     * Unserialize the action.
-     */
-    public function __wakeup()
-    {
-        if ($this->request instanceof SerializedRequest) {
-            $this->request = $this->request->unserialize();
-        }
-
-        Nova::resourcesIn(app_path('Nova'));
-    }
-
-    /**
      * Execute the action for the given request.
      *
      * @param  \Laravel\Nova\Http\Requests\ActionRequest $request
--- src/Actions/QueuedExport.php
+++ src/Actions/QueuedExport.php
@@ -2,7 +2,9 @@
 
 namespace Maatwebsite\LaravelNovaExcel\Actions;
 
+use Laravel\Nova\Nova;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Maatwebsite\LaravelNovaExcel\Requests\SerializedRequest;
 
 class QueuedExport extends ExportToExcel implements ShouldQueue
 {
@@ -10,4 +12,34 @@ class QueuedExport extends ExportToExcel implements ShouldQueue
      * @var string
      */
     public $name = 'Export to Excel';
+
+    /**
+     * Remove some attributes from this class when serializing,
+     * so the action can be queued as exportable.
+     * Serialize the request, so we keep information about
+     * the resource and lens in the queued jobs.
+     *
+     * @return array
+     */
+    public function __sleep()
+    {
+        if (!$this->request instanceof SerializedRequest) {
+            $this->request = SerializedRequest::serialize($this->request);
+        }
+
+        return ['headings', 'except', 'only', 'onlyIndexFields', 'request', 'resource'];
+    }
+
+    /**
+     * Deserialize the action.
+     */
+    public function __wakeup()
+    {
+        if ($this->request instanceof SerializedRequest) {
+            $this->request = $this->request->unserialize();
+        }
+
+        // Reload Nova resources
+        Nova::resourcesIn(app_path('Nova'));
+    }
 }
