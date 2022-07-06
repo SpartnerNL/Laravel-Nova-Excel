551726b042013117d623715cd1afd5b69522ee60
Sander Muller <sander@mulver.nl>
2022-07-06 16:26:28 +0200

    Remove file using a queued closure instead of app terminating
    See https://laravel.com/docs/9.x/queues#queueing-closures

--- src/Http/Controllers/ExcelController.php
+++ src/Http/Controllers/ExcelController.php
@@ -31,9 +31,9 @@ class ExcelController extends Controller
         $decryptedPath = decrypt($data['path']);
 
         if (config('excel.temporary_files.remote_disk')) {
-            app()->terminating(function () use ($decryptedPath) {
+            dispatch(function () use ($decryptedPath): void {
                 Storage::disk(config('excel.temporary_files.remote_disk'))->delete($decryptedPath);
-            });
+            })->delay(now()->addMinute());
 
             return Storage::disk(config('excel.temporary_files.remote_disk'))
                 ->download($decryptedPath, $data['filename']);
